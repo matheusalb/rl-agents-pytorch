5b5e6a7f3038543aa94a213a6928dec4793f147e
matheusalb <mvca@cin.ufpe.br>
2021-10-12 21:34:41 -0300

    updated run gk 3v3

--- run_ddpg_gk_3v3.sh
+++ run_ddpg_gk_3v3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in 0 1 2 3 4; do
-  python train_ddpg_gk_3v3.py --cuda -n "DDPG_VSSGK3V3-v0_0$i" -e VSSGK3V3-v0 
+  python train_ddpg_gk_3v3_fira.py --cuda -n "DDPG_VSSGK3V3_RSIMUPDATED-v0_0$i" -e VSSGK3V3-v0 --checkpoint models/gk_ft2.pth
 done
